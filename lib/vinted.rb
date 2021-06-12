e366017cd46b823f802b1bd7c02f5b2210dec370
Sahil Grover <sahil.grover@zalando.de>
2021-06-12 16:16:24 +0200

    parse file to create shipments

--- lib/vinted.rb
+++ lib/vinted.rb
@@ -5,6 +5,17 @@ module Vinted
   DEFAULT_FILE_NAME = 'input.txt'
 
   def self.run(file_name: )
-    P file_name || DEFAULT_FILE_NAME
+    shipments = read_input_file(file_name)
+    shipments.each do |shipment|
+      p shipment
+    end
+  end
+
+  def self.read_input_file(file_name)
+    result = []
+    file = File.open(File.join(ENV['PWD'], 'seed', (file_name || DEFAULT_FILE_NAME)))
+    result = file.map { |line| Vinted::Shipment.new(line.strip.chomp) }
+    file.close
+    return result
   end
 end
