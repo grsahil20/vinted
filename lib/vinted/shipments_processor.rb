b50239bcf85b203930c8c23a41fa3475073cb018
Sahil Grover <sahil.grover@zalando.de>
2021-06-13 20:43:25 +0200

    delete code

--- lib/vinted/shipments_processor.rb
+++ lib/vinted/shipments_processor.rb
@@ -5,14 +5,5 @@ module Vinted
 
     MAX_DISCOUNT = 10.00
 
-    def self.min_cost_for_size(size:)
-      costs = []
-      Vinted::ShipmentCost::DEFAULT_SHIPPING_COSTS.each do |provider, shipping_charges|
-        costs << shipping_charges[size.to_sym]
-      end
-      costs.min
-    end
-
-
   end
 end
--- spec/shipment_processor_spec.rb
+++ spec/shipment_processor_spec.rb
@@ -2,12 +2,4 @@ require "vinted/shipments_processor"
 
 describe Vinted::ShipmentsProcessor do
 
-  describe "#min_cost_for_size" do
-    it "returns relevnant lowest cost" do
-      expect(Vinted::ShipmentsProcessor.min_cost_for_size(size: "S")).to eq(1.50)
-      expect(Vinted::ShipmentsProcessor.min_cost_for_size(size: "M")).to eq(3.00)
-      expect(Vinted::ShipmentsProcessor.min_cost_for_size(size: "L")).to eq(4.00)
-    end
-  end
-
 end
